d4ab9273a4a235131d113028f471d7d9a578edc5
Nikolai <kolamba.r@gmail.com>
2023-11-23 13:53:26 +0300

    added .env.example made model for items and migration

--- products/models.py
+++ products/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Item(models.Model):
+    CUR = (
+        ('USD', 'доллары'),
+        ('RUB', 'рубли'),
+    )
+    name = models.CharField(max_length=100, verbose_name='Название', unique=True)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    price = models.IntegerField(verbose_name='цена')
+    currency = models.CharField(max_length=10, verbose_name='способ оплаты', choices=CUR)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
