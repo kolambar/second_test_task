8f4156756642778452417b585a262e66cdb65f80
Nikolai <kolamba.r@gmail.com>
2023-11-23 13:59:21 +0300

    admin has been set up for Item

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from products.models import Item
+
+
 # Register your models here.
+
+
+@admin.register(Item)
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'currency', )
+    list_filter = ('price', 'currency', )
+    search_fields = ('name', 'price', )
